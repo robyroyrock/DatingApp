e0c98dc393a98b7b20209bd0afdd70a1551448b7
robinsr87 <robin@aadspro.com>
2024-08-12 11:52:06 +0530

    End of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore;
+using API.Entities;
+namespace API.Data;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    
+        public DbSet<AppUser> Users {get;set;} 
+    
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+﻿//using API.Extensions;
+using Microsoft.AspNetCore.Identity;
+
+namespace API.Entities;
+
+public class AppUser 
+{
+    public int  Id { get; set; }
+    public required string UserName { get; set; }
+   
+}
